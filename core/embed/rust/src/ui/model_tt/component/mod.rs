0da77222d9168d59d9e2d25ea01a971fa9fcf3a6
Jan Pochyla <jpochyla@gmail.com>
2021-10-24 11:38:56 +0200

    chore(core): Add WiP Loader & generic Animation type

--- core/embed/rust/src/ui/model_tt/component/mod.rs
+++ core/embed/rust/src/ui/model_tt/component/mod.rs
@@ -1,5 +1,6 @@
 mod button;
 mod dialog;
+mod loader;
 mod page;
 mod passphrase;
 mod pin;
@@ -7,6 +8,7 @@ mod swipe;
 
 pub use button::{Button, ButtonContent, ButtonMsg, ButtonStyle, ButtonStyleSheet};
 pub use dialog::{Dialog, DialogMsg};
+pub use loader::{Loader, LoaderStyle, LoaderStyleSheet};
 pub use swipe::{Swipe, SwipeDirection};
 
 use super::{event, theme};
